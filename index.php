824892a85f6cff797f8247449b661ed0de51dd65
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2022-09-06 16:36:41 +0200

    refactor

--- index.php
+++ index.php
@@ -1,21 +1,7 @@
 <?php
 
-    echo 'test'
+
+    include './html/base.html'
 ?>
 
 
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Oscar PALISSOT application process</title>
-</head>
-<body>
-
-    <h1>test</h1>
-</body>
-
-</html>

89de2c4dd936185ee36fd1af6928014cfb1633e8
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2022-09-06 20:05:47 +0200

    update screen

--- src/screenShot.php
+++ src/screenShot.php
@@ -22,8 +22,3 @@ $api_url = $machine->generate_screenshot_api_url($options);
 
 //put link to your html code
 echo '<img src="' . $api_url . '">' . PHP_EOL;
-
-//or save screenshot as an image
-$output_file = 'output.png';
-file_put_contents($output_file, file_get_contents($api_url));
-echo 'Screenshot saved as ' . $output_file . PHP_EOL;

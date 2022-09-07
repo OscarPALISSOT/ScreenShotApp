07a0bda921bc291f827a2bdd7e9bd65e121dbad5
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2022-09-07 22:12:50 +0200

    ajax

--- js/form.js
+++ js/form.js
@@ -17,5 +17,26 @@ form.addEventListener( 'submit', e => {
         alert('Please, fill all the URLs.');
         return;
     }
-    alert('submit')
+    let formData = new FormData(form)
+    console.log(formData);
+    $.ajax({
+        url: form.getAttribute('action'),
+        type: form.getAttribute('method'),
+        processData: false,
+        contentType: false,
+        data: formData,
+        success: function(data, status)
+        {
+            console.log(data)
+            let html = '<h2>' + data.content + '</h2>';
+            $('.loading').html(null);
+            $('#ajax-modal').html(html);
+        },
+        error: function (xhr, ajaxOptions, thrownError)
+        {
+            $('.loading').html(null);
+            $('#ajax-modal').html('Error: ' + xhr.status);
+            console.log(thrownError);
+        }
+    })
 })

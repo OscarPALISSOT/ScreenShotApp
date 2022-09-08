45a316d07493e9e0758ff0737cbb150dd1796321
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2022-09-08 23:32:15 +0200

    google api uplaod

--- src/upload.php
+++ src/upload.php
@@ -6,16 +6,16 @@ require_once "googleAPI.php";
 use Google\Service\Drive;
 
 # TODO - PHP client currently chokes on fetching start page token
-function uploadBasic(){
+function uploadBasic($fileToUpload){
     try {
         $client = getClient();
         $client->addScope(Drive::DRIVE);
         $driveService = new Drive($client);
         // Create the file on your Google Drive
         $fileMetadata = new Google_Service_Drive_DriveFile(array(
-            'name' => 'a.jpg'));
-        $content = file_get_contents('a.jpg');
-        $mimeType=mime_content_type('a.jpg');
+            'name' => 'test'));
+        $content = file_get_contents($fileToUpload);
+        $mimeType=mime_content_type($fileToUpload);
         $file = $driveService->files->create($fileMetadata, array(
             'data' => $content,
             'mimeType' => $mimeType,
@@ -26,4 +26,4 @@ function uploadBasic(){
     }
 }
 
-uploadBasic();
+uploadBasic('a.jpg');

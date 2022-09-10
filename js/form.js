1f2ea131e801ce4c2bc48f0e13f009cc1d0b079b
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2022-09-10 19:43:54 +0200

    loading correctif

--- js/form.js
+++ js/form.js
@@ -20,6 +20,7 @@ form.addEventListener( 'submit', e => {
         return;
     }
     form.style.display = 'none';
+    response.style.display = 'block';
     let formData = $(form).serializeArray();
     $.ajax({
         url: form.getAttribute('action'),
@@ -31,8 +32,6 @@ form.addEventListener( 'submit', e => {
             let html = '<a target="_blank" rel="noopener noreferrer" href="' + data[0] + '">' + data[0] +'</a><a target="_blank" rel="noopener noreferrer" href="' + data[1] + '">' + data[1] +'</a><a target="_blank" rel="noopener noreferrer" href="' + data[2] + '">' + data[2] +'</a><a target="_blank" rel="noopener noreferrer" href="' + data[3] + '">' + data[3] +'</a><a target="_blank" rel="noopener noreferrer" href="' + data[4] + '">' + data[4] +'</a>';
             loading.style.display = 'none';
             responseContainer.innerHTML = html;
-            response.style.display = 'block';
-
         },
         error: function (xhr, ajaxOptions, thrownError)
         {
